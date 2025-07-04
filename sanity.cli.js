83cd88847f3fa9801c134b6fa0d089d7c1010245
ronaldr214 <rrangelramirez@gmail.com>
2025-07-04 18:37:16 -0500

    Create sanity.cli.js

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'TU_PROJECT_ID_AQUI', // Reemplazaremos esto después
+    dataset: 'production'
+  }
+})
