ad0a068cf4a0fa5800e8944ea6e9c0be31118178
ronaldr214 <rrangelramirez@gmail.com>
2025-07-04 18:42:33 -0500

    Create category.js

---
+++ schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'category',
+  title: 'Categorías',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Título de la Categoría',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Descripción',
+      type: 'text',
+    },
+  ],
+}
